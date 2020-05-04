bdc9052d6aa561c3357c81d3c98161f65d6fa96e
huyinghuan <ec.huyinghuan@gmail.com>
2020-05-04 22:24:04 +0800

    增加分页,提交评论

--- src/Comment.vue
+++ src/Comment.vue
@@ -1,6 +1,14 @@
 <template>
   <el-container>
     <el-main>
+      <el-form ref="form" :model="newComment">
+        <el-form-item>
+          <el-input type="textarea" v-model="newComment.content"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">发表评论</el-button>
+        </el-form-item>
+      </el-form>
       <el-card v-for="item in commentList" :key="item.comment.id">
         <div>
           <el-row>
@@ -21,13 +29,22 @@
         </div>
       </el-card>
     </el-main>
-    <el-footer></el-footer>
+    <el-footer>
+      <el-pagination
+        background
+        @current-change="handleCurrentChange"
+        :current-page.sync="pager.index"
+        :page-size="pager.pageSize"
+        layout="total, prev, pager, next"
+        :total="pager.total"
+      ></el-pagination>
+    </el-footer>
   </el-container>
 </template>
 
 <script>
 import { JX3BOX } from "@jx3box/jx3box-common";
-import { GET } from "./service";
+import { GET, POST } from "./service";
 // import { ElContainer, ElMain, ElFooter } from "element-ui";
 export default {
   name: "Comment",
@@ -40,17 +57,54 @@ export default {
   data: function() {
     return {
       commentList: [],
-      currentIndex: 1,
       defautlAvatar: JX3BOX.default_avatar,
-      profileLink: JX3BOX.__Links.dashboard.profile + "/"
+      profileLink: JX3BOX.__Links.dashboard.profile + "/",
+      newComment: {},
+      pager: {
+        index: 1,
+        pageSize: 10,
+        pageTotal: 1,
+        total: 0
+      }
     };
   },
-  mounted() {
-    GET(`/api/comments/post/${this.postId}/page/${this.currentIndex}`)
-      .then(resp => {
-        this.commentList = resp.data;
+  methods: {
+    reloadCommentList(index) {
+      GET(`/api/comments/post/${this.postId}/comment/page/${index}`)
+        .then(resp => {
+          this.commentList = resp.data;
+          this.pager = resp.page;
+        })
+        .catch(() => {});
+    },
+    handleCurrentChange(gotoIndex) {
+      this.reloadCommentList(gotoIndex);
+    },
+    onSubmit() {
+      POST(`/api/comments/post/${this.postId}/comment`, null, {
+        content: this.newComment.content
       })
-      .catch(() => {});
+        .then(() => {
+          this.$notify({
+            title: "",
+            message: "评论成功!",
+            type: "success",
+            duration: 3000,
+            position: "bottom-right"
+          });
+          if (
+            this.pager.index == this.pager.pageTotal ||
+            this.pager.total % this.pager.pageSize == 0
+          ) {
+            this.reloadCommentList(this.pager.index);
+          }
+        })
+        .catch(() => {});
+    }
+  },
+
+  mounted() {
+    this.reloadCommentList(1);
   }
 };
 </script>
--- src/service.js
+++ src/service.js
@@ -1,3 +1,5 @@
+import { Notification } from 'element-ui';
+import { JX3BOX } from "@jx3box/jx3box-common"
 export const GET = function (url, queryParams) {
     let options = {
         "method": "GET",
@@ -49,15 +51,35 @@ function __fetch(url, queryParams, options) {
             case 200:
                 break
             case 401:
-
+                window.location.href = JX3BOX.__Links.account.login + "?redirect=" + encodeURIComponent(window.location.href)
                 throw new Error("错误:" + resp.statusText)
             case 403:
-
+                window.location.href = JX3BOX.__Links.account.login + "?redirect=" + encodeURIComponent(window.location.href)
+                throw new Error("错误:" + resp.statusText)
+            case 423:
+                window.location.href = JX3BOX.__Links.account.email_verify + "?redirect=" + encodeURIComponent(window.location.href)
                 throw new Error("错误:" + resp.statusText)
             case 406:
+                resp.text().then((body) => {
+                    Notification.warning({
+                        title: "系统",
+                        message: body || "提交内容不合法,请重新提交",
+                        type: "success",
+                        duration: 3000,
+                        position: "bottom-right"
+                    })
+                })
 
                 throw new Error("错误:" + resp.statusText)
             default:
+                resp.text().then((body) => {
+                    Notification.error({
+                        title: "系统:" + resp.statusText,
+                        message: body || "系统错误,请稍后重试!",
+                        duration: 3000,
+                        position: "bottom-right"
+                    })
+                })
 
                 throw new Error("错误:" + resp.statusText)
         }
