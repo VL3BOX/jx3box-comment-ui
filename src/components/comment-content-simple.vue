752ab8d70b70347a87425958c11494fe74410cbf
flyflyknife <small.fly.knife@gmail.com>
2020-11-13 17:31:47 +0800

    #8 修复评论的回复无法换行

--- src/components/comment-content-simple.vue
+++ src/components/comment-content-simple.vue
@@ -1,108 +1,116 @@
 <template>
-    <div class="u-reply">
-        <div class="u-reply-content">
-            <span class="u-reply-label" v-if="replyForUserId != 0">
-                回复
-                <el-link
-                    type="primary"
-                    target="_blank"
-                    :href="userHref"
-                    >@{{ replyForUsername }}</el-link
-                >
-                :
-            </span>
-            {{ content }}
-        </div>
-        <div class="u-attachements" v-if="attachments.length">
-            <el-image
-                v-for="url in attachments"
-                :key="url"
-                :src="url + '?x-oss-process=style/comment_thumb'"
-                :preview-src-list="attachments"
-                lazy
-            ></el-image>
-        </div>
-        <div class="u-toolbar">
-            <el-button
-                class="u-admin"
-                v-if="canDelete"
-                type="text"
-                icon="el-icon-delete"
-                size="mini"
-                @click="deleteComment()"
-                >删除</el-button
-            >
-            <el-button
-                class="u-admin"
-                v-if="canReply"
-                type="text"
-                icon="el-icon-chat-line-round"
-                size="mini"
-                @click="showReplyForReplyInput()"
-                >回复</el-button
-            >
-            <time class="u-date">
-                <i class="el-icon-time"></i>
-                {{ dataFormat(date) }}
-            </time>
-        </div>
+  <div class="u-reply">
+    <div class="u-reply-content">
+      <span class="u-reply-label" v-if="replyForUserId != 0">
+        回复
+        <el-link type="primary" target="_blank" :href="userHref"
+          >@{{ replyForUsername }}</el-link
+        >
+        :
+      </span>
+      <p v-for="(p, index) in getPList(content)" :key="index">{{ p }}</p>
     </div>
+    <div class="u-attachements" v-if="attachments.length">
+      <el-image
+        v-for="url in attachments"
+        :key="url"
+        :src="url + '?x-oss-process=style/comment_thumb'"
+        :preview-src-list="attachments"
+        lazy
+      ></el-image>
+    </div>
+    <div class="u-toolbar">
+      <el-button
+        class="u-admin"
+        v-if="canDelete"
+        type="text"
+        icon="el-icon-delete"
+        size="mini"
+        @click="deleteComment()"
+        >删除</el-button
+      >
+      <el-button
+        class="u-admin"
+        v-if="canReply"
+        type="text"
+        icon="el-icon-chat-line-round"
+        size="mini"
+        @click="showReplyForReplyInput()"
+        >回复</el-button
+      >
+      <time class="u-date">
+        <i class="el-icon-time"></i>
+        {{ dataFormat(date) }}
+      </time>
+    </div>
+  </div>
 </template>
 
 <script>
 import { Utils } from "@jx3box/jx3box-common";
 function fillZero(num) {
-    return num > 9 ? num : `0${num}`;
+  return num > 9 ? num : `0${num}`;
 }
 export default {
-    props: [
-        "commentId",
-        "content",
-        "attachments",
-        "date",
-        "hasReply",
-        "canDelete",
-        "canReply",
-        "userHref",
-        "replyForUsername",
-        "replyForUserId",
-    ],
-    data: function() {
-        return {
-            showInput: false,
-        };
+  props: [
+    "commentId",
+    "content",
+    "attachments",
+    "date",
+    "hasReply",
+    "canDelete",
+    "canReply",
+    "userHref",
+    "replyForUsername",
+    "replyForUserId",
+  ],
+  data: function () {
+    return {
+      showInput: false,
+    };
+  },
+  filters: {
+    profileLink: function (uid) {
+      return Utils.authorLink(uid);
     },
-    filters: {
-        profileLink: function(uid) {
-            return Utils.authorLink(uid);
-        },
+  },
+  methods: {
+    getPList(content) {
+      return content.split("\n");
     },
-    methods: {
-        deleteComment() {
-            this.$emit("delete", this.commentId);
-        },
-        dataFormat(str) {
-            let d = new Date(str);
-            return (
-                d.getFullYear() +
-                "-" +
-                fillZero(d.getMonth() + 1) +
-                "-" +
-                fillZero(d.getDate()) +
-                " " +
-                fillZero(d.getHours()) +
-                ":" +
-                fillZero(d.getMinutes()) +
-                ":" +
-                fillZero(d.getSeconds())
-            );
-        },
-        showReplyForReplyInput() {
-            this.$emit("showReplyInput");
-        },
+    deleteComment() {
+      this.$emit("delete", this.commentId);
     },
+    dataFormat(str) {
+      let d = new Date(str);
+      return (
+        d.getFullYear() +
+        "-" +
+        fillZero(d.getMonth() + 1) +
+        "-" +
+        fillZero(d.getDate()) +
+        " " +
+        fillZero(d.getHours()) +
+        ":" +
+        fillZero(d.getMinutes()) +
+        ":" +
+        fillZero(d.getSeconds())
+      );
+    },
+    showReplyForReplyInput() {
+      this.$emit("showReplyInput");
+    },
+  },
 };
 </script>
 
 <style lang="less">
+.u-reply-content {
+  p {
+    padding: 0;
+    margin: 0;
+    line-height: 1.75;
+    white-space: pre-wrap;
+  }
+}
 </style>
