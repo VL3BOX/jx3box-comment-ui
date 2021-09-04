eebcb4afab205e1a2adecf6d331c374032f5b803
iRuxu <rx6@qq.com>
2021-09-05 02:23:46 +0800

    fix api

--- src/service.js
+++ src/service.js
@@ -67,8 +67,7 @@ export const DELETE = function (url, queryParams) {
 }
 
 function __fetch(url, queryParams, options) {
-    // let domain = process.env.NODE_ENV == "production" ? JX3BOX.__next : "/"  //TODO:待统一
-    let domain = process.env.NODE_ENV == "production" ? 'https://next2.jx3box.com/' : "/"
+    let domain = process.env.NODE_ENV == "production" ? JX3BOX.__next2 : "/"
     if (domain[domain.length - 1] == "/") {
         domain = domain.substring(0, domain.length - 1)
     }
@@ -79,8 +78,7 @@ function __fetch(url, queryParams, options) {
         Object.keys(queryParams).forEach((key) => {
             queryQueue.push(key + "=" + queryParams[key])
         })
-        // let domain = JX3BOX.__next
-        let domain = 'https://next2.jx3box.com/'
+        let domain = JX3BOX.__next2
         if (domain[domain.length - 1] == "/") {
             domain = domain.substring(0, domain.length - 1)
         }
